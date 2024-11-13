764a263405bb0df23a2a8423331b5bc00966c624
“Cristoge” <cristo.g@outlook.es>
2024-11-13 20:24:26 +0100

    Ajustada la organización de carpetas

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -13,7 +13,6 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
-  
 
   -- change trouble config
   {
@@ -36,27 +35,6 @@ return {
   },
 
   -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
 
   -- add pyright to lspconfig
   {
