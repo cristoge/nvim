2d37ea11953a31286a3a953f9d2148b077d57129
“Cristoge” <cristo.g@outlook.es>
2024-11-13 18:49:27 +0100

    Colores y primeros Plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,44 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     --import any extra modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
+    --{ import = "lazyvim.plugins.extras.ai.copilot" },
+    {
+      "CRAG666/code_runner.nvim",
+      config = function()
+        require("code_runner").setup({
+          filetype = {
+            java = {
+              "cd $dir &&",
+              "javac $fileName &&",
+              "java $fileNameWithoutExt",
+            },
+            python = "python3 -u",
+            typescript = "ts-node",
+            rust = {
+              "cd $dir &&",
+              "rustc $fileName &&",
+              "$dir/$fileNameWithoutExt",
+            },
+            c = function(...)
+              c_base = {
+                "cd $dir &&",
+                "gcc $fileName -o",
+                "/tmp/$fileNameWithoutExt",
+              }
+              local c_exec = {
+                "&& /tmp/$fileNameWithoutExt &&",
+                "rm /tmp/$fileNameWithoutExt",
+              }
+              vim.ui.input({ prompt = "Add more args:" }, function(input)
+                c_base[4] = input
+                vim.print(vim.tbl_extend("force", c_base, c_exec))
+                require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+              end)
+            end,
+          },
+        })
+      end,
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -6,12 +6,32 @@ return {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
   },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      })
+    end,
+  },
+  {
+    "b0o/lavi.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+  },
+  {
+    "embark-theme/vim",
+    as = "embark",
+  },
 
   -- somewhere in your config
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_vivid",
+      colorscheme = "embark",
     },
   },
 }
